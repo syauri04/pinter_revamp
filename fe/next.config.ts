06dfd1622379fc62900d0db626995406b817a75f
syauri04 <syauri04@gmail.com>
2025-11-05 19:56:42 +0700

    update geojson

--- fe/next.config.ts
+++ fe/next.config.ts
@@ -1,22 +1,5 @@
 import type { NextConfig } from "next";
 
-// const nextConfig: NextConfig = {
-//   images: {
-//     remotePatterns: [
-//       {
-//         protocol: "http",
-//         hostname: "localhost",
-//         port: "1337",
-//         pathname: "/uploads/**",
-//       },
-//     ],
-//   },
-// };
-
-// export default nextConfig;
-
-// FOR SERVER
-// root@pinter:~/project/pinter_revamp/fe# cat next.config.ts
 const nextConfig = {
   images: {
     unoptimized: true,
